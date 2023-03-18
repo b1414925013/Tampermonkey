df3ec162138d45a200b40a58e20a27c35a85bf0e
jfbian <1414925013@qq.com>
2023-03-18 16:11:34 +0800

    001

---
+++ bbb.js
@@ -0,0 +1,11 @@
+// const REPORT_SHEET_NAME = "员工绩效";
+
+// const workbook = XLSX.readFile("C:\\Users\\Administrator\\Desktop\\asdas\\aaa.xlsx", {});
+// const worksheet = workbook.Sheets[REPORT_SHEET_NAME];
+// const sheetJSONArray = XLSX.utils.sheet_to_json(worksheet);
+
+// console.log(sheetJSONArray)
+
+
+alert('test')
+
